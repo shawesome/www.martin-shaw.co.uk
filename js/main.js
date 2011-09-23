ea414a934998447ae28283145a5bacbdc82b7766
Martin Shaw <martin.shaw@meanbee.com>
2011-09-23 18:05:44 +0100

    Searching implemented, they simpled stop moving when in a given distance

--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
-var linkCnt = 0;
-var linkLimit = 30;
-var links = [];
-var fps = 60;
+// global variables to configure the battle
+
+// arrays of the links 
+var links = Array();
+var red_links = Array();
+var blue_links = Array();
+
+// engine variables
+var fps = 30;
+
+// link variables
+var spacing = 30;
+var link_cnt = 0;
+var link_limit = 30;
+var move_speed = 1;
+
+// searching
+var perform_search = 30;
+var approach_distance = 300;
+
+
+
 $(function() {
-    // generate linkLimit links to place in the battle.
-    for (var i = 0; i < linkLimit; i++) {
-        var colour = (i % 2 == 0) ? 'red': 'blue';
-        links.push(add_link(colour));
+    // generate link_limit links to place in the battle.
+    for (var i = 0; i < link_limit/2; i++) {
+        var new_link = add_link('red', i);
+        links.push(new_link)   
+        red_links.push(new_link)   
+    }
+    for (var i = 0; i < link_limit/2; i++) {
+        var new_link = add_link('blue', i);
+        links.push(new_link)   
+        blue_links.push(new_link)   
     }
 
     // function which loops indefinitely
@@ -50,14 +74,18 @@ $(function() {
     window.onEachFrame(game_loop);
 
     // randomly adds a link to the battle. positions based on colour.
-    function add_link (colour) {
+    function add_link (colour, index) {
         var battle = $('div#battle');
-        var id = 'link-' + ++linkCnt;
-        var left = (colour == 'blue') ? Math.random() * 100: Math.random() * 100 + (document.width - 150);
-        var top = Math.random() * (document.height - 50);
-        var orientation = (colour == 'blue') ? 'right': 'left';
+        var link = new Array();
+        link['colour'] = colour;
+
+        var id = 'link-' + ++link_cnt;
+        var left = (colour == 'blue') ? -30 - index * spacing: document.width + index * spacing;
+        var top = Math.random() * (document.height - 30);
+        link['orientation'] = (colour == 'blue') ? 'right': 'left';
+        link['action'] = 'walk';
         battle.append($('<div/>')
-            .addClass('link ' + colour + ' walk-' + orientation)
+            .addClass('link ' + colour + ' ' + link.action + '-' + link.orientation)
             .attr('id', id)
             .css({
                 'z-index': Math.floor(top),
@@ -65,17 +93,72 @@ $(function() {
                 'top': top +'px',
             }));
 
-        return $('#' + id);
+        link['object'] = $('#' + id);
+        link['status'] = 'searching';
+        //searching is costly so divide it equally
+
+        link['search_tick'] = Math.floor(((perform_search / (link_limit / 2)) * i) % perform_search);
+
+
+        return link;
     }
 
     function update() {
-        for (var i = 0; i < linkCnt; i++) {
-            var move = (i % 2 == 0) ? '-=1' : '+=1';
-            links[i].animate({
-                'left': move,
-            }, 1000/fps);
+        for (var i = 0; i < link_cnt; i++) {
+            do_behaviour(links[i]);
         }
     }
 
+    function do_behaviour(link) {
+        switch(link.status) {
+            case 'searching':
+                search(link);
+                break;
+            case 'approaching':
+                approach(link);
+                break;
+
+        }
+        
+    }
+
+    function search(link) {
+        // see if anyone is in range
+        link.search_tick++;
+        if (link.search_tick == perform_search) {
+            var target_links = (link.colour == 'blue') ? red_links: blue_links;
+            var x1 = link.object.position().left + link.object.width() / 2;
+            var y1 = link.object.position().top + link.object.height() / 2;
+
+            for (var i = 0; i < link_limit / 2; i++) {
+                var x2 = target_links[i].object.position().left + target_links[i].object.width() / 2;
+                var y2 = target_links[i].object.position().top + target_links[i].object.height() / 2;
+                var dx = x1 - x2;
+                var dy = y1 - y2;
+
+                var distance = Math.sqrt(dx * dx + dy * dy);
+                
+                if (distance < approach_distance) {
+                    console.log(distance);
+                    link.status = 'approaching';
+                    link.target = target_links[i];
+                    return;
+                }
+
+            }
+            link.search_tick = 0;
+        }
+        
+        // no one is in range so just keep moving
+        var move = (link.colour == 'blue') ? '+=' + move_speed : '-=' + move_speed;
+        link.object.animate({
+            'left': move,
+        }, fps/1000);
+    }
+
+
+    function approach(link) {
+        
+    }
 
 });
